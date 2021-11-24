ca20c9f5c6a4401e91d14c02dc61d364d7f69840
harshil-shah9900 <harshil.shah9900@gmail.com>
2021-11-24 14:11:33 +0000

    Update mips_cpu_bus.v

--- rtl/mips_cpu_bus.v
+++ rtl/mips_cpu_bus.v
@@ -138,7 +138,7 @@ typedef enum logics[1:0]
 
     //  Regisgters
         //  General Registers
-        logic[31:0] register [31:0];
+        logic[31:0][31:0] registers;
         //  Special Registers
         logic[31:0] HI;
         logic[31:0] LO;
