ab867b6e10ae4ec1650a35d83cce427c9dbeedf4
lc2920 <94857891+lc2920@users.noreply.github.com>
2021-12-06 21:03:43 +0000

    Update tb_cpu.v

--- Test/Test_Area/tb_cpu.v
+++ Test/Test_Area/tb_cpu.v
@@ -43,39 +43,38 @@ module tb_cpu();
         $finish(0);
     end
 
-    mips_cpu_bus mips_cpu_bus(  .clk(clk), 
-                                .reset(reset), 
-                                .active(active), 
-                                .register_v0(register_v0), 
-                                .address(address), 
-                                .write(write), 
-                                .read(read), 
-                                .waitrequest(waitrequest), 
-                                .writedata(writedata), 
-                                .byteenable(byteenable), 
-                                .readdata(readdata));
+    mips_cpu_bus mips_cpu_bus(.clk(clk), .reset(reset), .active(active), .register_v0(register_v0), .address(address), 
+    .write(write), .read(read), .waitrequest(waitrequest), .writedata(writedata), .byteenable(byteenable), .readdata(readdata));
     initial begin
         waitrequest=0;
         reset=0;
         /*repeat (30) begin
-            $display("active\t\t%d",active);
-            $display("address\t\t%d",address);
-            $display("write\t\t%d",write);
-            $display("read\t\t%d",read);
+            $display("active %d",active);
+            $display("address %d",address);
+            $display("write %d",write);
+            $display("read %d",read);
             $display("writedata %d",writedata);
-            $display("byteenable\t%d",byteenable);
+            $display("byteenable %d",byteenable);
             $display("readdata %d",readdata);
             $display(" ");
-            #1;
+            #2;
         end*/
+
+        /*repeat (30) begin
+            $display("%d %d %d %d",wd1,wd2,wd3,wd4);
+            $display("%d %d %d %d",rd1,rd2,rd3,rd4);
+            $display("%d %d",write,writedata);
+            #2;
+        end*/
+
+        #500;
         passed=1;
-        for(int i=0;i<200;i++) begin
+        for(int i=0;i<20;i++) begin
             if (RAM[i]!=TESTRAM[i]) begin
                 $display("RAM %d expected %d given %d", i, TESTRAM[i],RAM[i]); 
                 passed=0;
             end
         end
-        #500;
 
         if (passed==1) begin
             $display("%s passed",RAM_FILE);
@@ -87,27 +86,32 @@ module tb_cpu();
 
     assign rdata = (address > 3217031167) ? {RAM[address-3217031066],RAM[address-3217031067],RAM[address-3217031068],RAM[address-3217031069]} : {RAM[address+2],RAM[address+1],RAM[address],RAM[address-1]}; //1111111100001111
 
-    logic [7:0] rd1, rd2, rd3, rd4, wd1, wd2, wd3, wd4;
+    logic [7:0] rd1, rd2, rd3, rd4;//, wd1, wd2, wd3, wd4;
+    logic a;
 
     assign rd1 = rdata[31:24]&{8{byteenable[3]}};
     assign rd2 = rdata[23:16]&{8{byteenable[2]}};
     assign rd3 = rdata[15:8]&{8{byteenable[1]}};
     assign rd4 = rdata[7:0]&{8{byteenable[0]}};
 
-    assign wd1 = writedata[31:24]&{8{byteenable[3]}};
+    /*assign wd1 = writedata[31:24]&{8{byteenable[3]}};
     assign wd2 = writedata[23:16]&{8{byteenable[2]}};
     assign wd3 = writedata[15:8]&{8{byteenable[1]}};
-    assign wd4 = writedata[7:0]&{8{byteenable[0]}};
+    assign wd4 = writedata[7:0]&{8{byteenable[0]}};*/
 
-    always_comb begin
+   always_comb begin
         if (read) begin
             readdata = {rd1,rd2,rd3,rd4};
         end
         if (write) begin
-            RAM[address-1] = wd4;
-            RAM[address] = wd3;
-            RAM[address+1] = wd2;
-            RAM[address+2] = wd1;
+            a = byteenable;
+            RAM[address-1] = writedata*a;
+            a = byteenable/2;
+            RAM[address] = (writedata/256)*a;
+            a = byteenable/4;
+            RAM[address+1] = (writedata/65536)*a;
+            a = byteenable/8;
+            RAM[address+2] = (writedata/16777216)*a;
         end
     end
 
