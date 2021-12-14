dce421a3c9afa2c06f78da930ce093e2a2f08d31
harshil-shah9900 <harshil.shah9900@gmail.com>
2021-12-14 16:51:34 +0000

    Reshuffling

--- Test/test_mips_cpu_bus.sh
+++ Test/test_mips_cpu_bus.sh
@@ -8,11 +8,10 @@
 #
 #
 #!/bin/bash
+
 #set -eou pipefail
 #$1 is absolute or relative
 
-#echo $1
-#echo $2 which is the instruction to be tested
 
 #$1 is the location of a cpu.v
 #we must recompile for every instruction (iterate through)
@@ -29,7 +28,7 @@ elif [ $# -eq 2 ]
 then
     testcases="./test/testcasesnew/${2^^}*_test.txt" #IF YOU HAVE MULTIPLE TEST CASES, YOU MAY NEED TO ADD AN ASTERISK, LIKE _*
 else
-    echo too many/few arguments
+    #echo too many/few arguments
     exit 1
 fi
 
@@ -39,39 +38,37 @@ fi
 
 for test in $testcases #directory containing all the RAM outputs so we can compare against. iterate since it could be one, or all
     do
-    #
+    #echo $test
     testname=$(basename ${test} _test.txt)
-    echo $test
-    expected_dir="${testname}_expected.txt"
-    test_dir="${testname}_test.txt"
+    
+    expected_dir="./test/testcasesnew/${testname}_expected.txt"
+    
     test_id_lowercase=${testname,,}
     test_lowercase=${test_id_lowercase%_*}
+
     #echo $test_dir
     #echo $expected_dir
+
     echo -n ${test_id_lowercase} 
     echo -n " "
     echo -n ${test_lowercase} 
     echo -n " "
 
 
-
-    iverilog -Wall -g 2012 -s tb_cpu -o test/tb_cpu \
+    iverilog -Wall -g 2012 -s tb_cpu -o ./test/tb_cpu \
     -P tb_cpu.INPUT_FILE=\"${test}\" \
     -P tb_cpu.EXPECTED_FILE=\"${expected_dir}\" \
-    test/tb_cpu.v ${1}/*.v  > /dev/null 2>&1 #change mips_cpu_bus to *.v
+    ./test/tb_cpu.v ${1}/*.v  > /dev/null 2>&1 #change mips_cpu_bus to *.v
     
-    #cd /test/Test_Area
     ./test/tb_cpu
-    #cd ..
-    #cd ..
+
 
     #this should output with display? Ok - i really don't know how it's handled. I'll assume it can fail on the execution level
     #except it can't it really can't. So all the pass fail is inside the testbench I think
+    #actually it can fail at execution and print some bs to stdout, 
+    #if readmemh fails, but compilation is successful. it'll say unable to open x for reading. 
+    #but nothing I can do about that except just make it work in the first place
 
-    #so the problem I have is that I don't want to have to be in the directory of test bench to run it. It will have to deal with the fact that
-    #the cwd will be root, not /test
-
-    #echo test
     done
 
 
